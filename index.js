d4a82f1b3802ee27ea4cf585a33d1c5f8318f4a7
NathaliePatzer <nathalielaise@gmail.com>
2025-11-06 21:14:38 -0300

    :sparkles: table steps resolution

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 
     var cont = 0;
+    var countSteps = 0;
+    var isFinished = false;
     var inputStack = [];
     var stack = [];
     var parsingTable = {
@@ -28,106 +30,181 @@ $(document).ready(function () {
     };
 
     function createInputStack(inputString) {
-        //Converte a string em um array de caracteres:
+        //Converte a string em um array de caracteres
         let charArray = [...inputString];
 
-        //Inverte a ordem do array:
+        //Inverte a ordem do array
         let reversedArray = charArray.reverse();
 
-        //Retorna um novo array começando com "$" seguido pelos elementos do array invertido:
+        //Retorna um novo array começando com "$" seguido pelos elementos do array invertido
         return ['$', ...reversedArray];
     }
 
     function stackToString(stack) {
-        // Primeiro, revertemos o array para que a ordem fique correta.
-        // Ex: ['$', 'd', 'c', 'b', 'a'] se torna ['a', 'b', 'c', 'd', '$']
+        // Reverte o array para que a ordem fique correta
         const reversedArray = stack.slice().reverse();
 
-        // Depois, juntamos todos os elementos do array revertido em uma única string.
+        // Junta todos os elementos do array revertido em uma única string
         const resultString = reversedArray.join('');
 
         return resultString;
     }
 
-    function sintaxAnalyzer() {
-        cont++;
-        var stackItem = stack[stack.length - 1];
-        var inputStackItem = inputStack[inputStack.length - 1];
+    function clear() {
+        cont = 0;
+        stack = [];
+        inputStack = [];
+        countSteps = 0;
+        isFinished = false;
+    }
 
-        console.log(stackItem);
-        console.log(inputStackItem);
-        console.log(inputStack);
-        console.log(stack);
+    $("#button-clear").click(function () {
+        clear();
+        $("#words").val("");
+        var tableEntry = `<tr class="row">
+            <td class="column_stack">-</td>
+            <td class="column_entry">-</td>
+            <td class="column_action">-</td>
+        </tr>`;
+        $("#resolutionTableBody").html(tableEntry);
+    });
 
-        if (stackItem == inputStackItem) {
-            if (stackItem == "$") {
-                var tableEntry = `<tr class="row">
+    $("#button-step").click(function () {
+        if ($("#words").val().trim() != "" && !isFinished) {
+            if (countSteps == 0) {
+                cont = 0;
+                stack[0] = "$";
+                stack[1] = "S";
+
+                inputStack = createInputStack($("#words").val());
+                $("#resolutionTableBody").html("");
+            }
+
+            sintaxAnalyzer(true);
+            countSteps++;
+        } else if (isFinished){
+            $.magnificPopup.open({
+                items: {
+                    src: '#finished-popup',
+                    type: 'inline'
+                },
+                callbacks: {
+                    open: function () {
+                        setTimeout(function () {
+                            $.magnificPopup.close();
+                        }, 4000);
+                    }
+                },
+                removalDelay: 300,
+                mainClass: 'mfp-fade'
+            });
+        } else {
+            $.magnificPopup.open({
+                items: {
+                    src: '#empty-popup',
+                    type: 'inline'
+                },
+                callbacks: {
+                    open: function () {
+                        setTimeout(function () {
+                            $.magnificPopup.close();
+                        }, 4000);
+                    }
+                },
+                removalDelay: 300,
+                mainClass: 'mfp-fade'
+            });
+        }
+    });
+
+    function sintaxAnalyzer(isStepByStep) {
+        if (!isFinished) {
+            cont++;
+            var stackItem = stack[stack.length - 1];
+            var inputStackItem = inputStack[inputStack.length - 1];
+
+            console.log(stackItem);
+            console.log(inputStackItem);
+            console.log(inputStack);
+            console.log(stack);
+
+            if (stackItem == inputStackItem) {
+                if (stackItem == "$") {
+                    var tableEntry = `<tr class="row">
                     <td class="column_stack">${stack.join('')}</td>
                     <td class="column_entry">${stackToString(inputStack)}</td>
-                    <td class="column_action">Aceito em ${cont} passos</td>
+                    <td class="column_action">Aceito em ${cont} passo${cont > 1 ? "s" : ""}</td>
                 </tr>`;
-                stack.pop();
-                inputStack.pop();
-                $("#resolutionTableBody").append(tableEntry);
-                //alert("aceito em X passos");
-            } else {
-                var tableEntry = `<tr class="row">
+                    stack.pop();
+                    inputStack.pop();
+                    $("#resolutionTableBody").append(tableEntry);
+                    isFinished = true;
+                    //alert("aceito em X passos");
+                } else {
+                    var tableEntry = `<tr class="row">
                     <td class="column_stack">${stack.join('')}</td>
                     <td class="column_entry">${stackToString(inputStack)}</td>
                     <td class="column_action">Ler ${inputStackItem}</td>
                 </tr>`;
-                stack.pop();
-                inputStack.pop();
-                $("#resolutionTableBody").append(tableEntry);
-                //alert("ler");
-                sintaxAnalyzer();
-            }
-        } else {
-            if (stackItem in parsingTable) {
-                //alert("esta na parsing table");
-                if (inputStackItem in parsingTable[stackItem]) {
-                    var tableEntry = `<tr class="row">
+                    stack.pop();
+                    inputStack.pop();
+                    $("#resolutionTableBody").append(tableEntry);
+                    //alert("ler");
+                    if (!isStepByStep) {
+                        sintaxAnalyzer(false);
+                    }
+                }
+            } else {
+                if (stackItem in parsingTable) {
+                    //alert("esta na parsing table");
+                    if (inputStackItem in parsingTable[stackItem]) {
+                        var tableEntry = `<tr class="row">
                         <td class="column_stack">${stack.join('')}</td>
                         <td class="column_entry">${stackToString(inputStack)}</td>
                         <td class="column_action">${stackItem} → ${parsingTable[stackItem][inputStackItem]}</td>
                     </tr>`;
 
-                    stack.pop();
-                    let productionRule = parsingTable[stackItem][inputStackItem];
+                        stack.pop();
+                        let productionRule = parsingTable[stackItem][inputStackItem];
 
-                    if (productionRule !== "ε") {
-                        for (let i = productionRule.length - 1; i >= 0; i--) {
-                            // Empilhamos cada caractere
-                            stack.push(productionRule[i]);
+                        if (productionRule !== "ε") {
+                            for (let i = productionRule.length - 1; i >= 0; i--) {
+                                // Empilha cada caractere
+                                stack.push(productionRule[i]);
+                            }
                         }
-                    }
-                    $("#resolutionTableBody").append(tableEntry);
-                    //alert("tem na parsing table");
-                    sintaxAnalyzer();
-                } else {
-                    var tableEntry = `<tr class="row">
+                        $("#resolutionTableBody").append(tableEntry);
+                        //alert("tem na parsing table");
+                        if (!isStepByStep) {
+                            sintaxAnalyzer(false);
+                        }
+                    } else {
+                        var tableEntry = `<tr class="row">
                         <td class="column_stack">${stack.join('')}</td>
                         <td class="column_entry">${stackToString(inputStack)}</td>
-                        <td class="column_action">Erro em ${cont} passos</td>
+                        <td class="column_action">Erro em ${cont} passo${cont > 1 ? "s" : ""}</td>
                     </tr>`;
-                    $("#resolutionTableBody").append(tableEntry);
-                    //alert("erro (topo da pilha não esta na parsing table");
-                }
-            } else {
-                var tableEntry = `<tr class="row">
+                        $("#resolutionTableBody").append(tableEntry);
+                        //alert("erro (topo da pilha não esta na parsing table");
+                        isFinished = true;
+                    }
+                } else {
+                    var tableEntry = `<tr class="row">
                     <td class="column_stack">${stack.join('')}</td>
                     <td class="column_entry">${stackToString(inputStack)}</td>
-                    <td class="column_action">Erro em ${cont} passos</td>
+                    <td class="column_action">Erro em ${cont} passo${cont > 1 ? "s" : ""}</td>
                 </tr>`;
-                $("#resolutionTableBody").append(tableEntry);
-                //alert("erro (topo da pilha não esta na parsing table");
+                    $("#resolutionTableBody").append(tableEntry);
+                    //alert("erro (topo da pilha não esta na parsing table");
+                    isFinished = true;
+                }
             }
         }
     }
 
     //Criar a pilha da entrada 
     $("#button-execute").click(function () {
-        if ($("#words").val().trim() != "") {
+        if ($("#words").val().trim() != "" && !isFinished) {
             cont = 0;
             stack[0] = "$";
             stack[1] = "S";
@@ -135,8 +212,24 @@ $(document).ready(function () {
             inputStack = createInputStack($("#words").val());
 
             $("#resolutionTableBody").html("");
-            sintaxAnalyzer();
+            sintaxAnalyzer(false);
 
+        } else if (isFinished){
+            $.magnificPopup.open({
+                items: {
+                    src: '#finished-popup',
+                    type: 'inline'
+                },
+                callbacks: {
+                    open: function () {
+                        setTimeout(function () {
+                            $.magnificPopup.close();
+                        }, 4000);
+                    }
+                },
+                removalDelay: 300,
+                mainClass: 'mfp-fade'
+            });
         } else {
             $.magnificPopup.open({
                 items: {
@@ -181,6 +274,7 @@ $(document).ready(function () {
 
     //verificar caracteres digitados 
     $("#words").keydown(function (event) {
+        clear();
         var text = $("#words").val();
         console.log(event.key + ":" + event.keyCode)
         if ((event.keyCode < 65 || event.keyCode > 68) && event.keyCode != 8) {
@@ -217,7 +311,7 @@ $(document).ready(function () {
     });
 
 
-    //conteudo clicado
+    //conteúdo clicado
     $(".column_button_click").click(function () {
         var input = $("#sentence-input").val();
         var token = this.textContent;
